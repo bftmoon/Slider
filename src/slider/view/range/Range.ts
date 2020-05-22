9a8ee753143b44b69e1661cb6e2e9a0cee411e7d
bigfatmoon <a21mor@mail.ru>
2020-05-22 21:31:30 +0300

    Range position setter update. Scale build update

--- src/slider/view/range/Range.ts
+++ src/slider/view/range/Range.ts
@@ -2,26 +2,28 @@ import IViewElement from "../IViewElement";
 import CssClassUtil from "../CssClassUtil";
 
 class Range implements IViewElement {
-    element: HTMLDivElement;
+  element: HTMLDivElement;
 
-    buildHtml(isVertical: boolean, percentMin: number, percentMax: number, rootElement?: HTMLElement): HTMLElement {
-        this.element = document.createElement('div');
-        CssClassUtil.initClass(this, isVertical);
-        this.updatePosition(isVertical, percentMin, percentMax, rootElement);
-        return this.element;
+  buildHtml(isVertical: boolean): HTMLElement {
+    this.element = document.createElement('div');
+    CssClassUtil.initClass(this, isVertical);
+    return this.element;
+  }
+
+  toggle() {
+    CssClassUtil.toggleHidden(this);
+  }
+
+  updatePosition(isVertical: boolean, percent: { min?: number; max?: number }) {
+    if (isVertical) {
+      const heightPart = this.element.parentElement.clientHeight / 100;
+      if (percent.min !== undefined) this.element.style.marginTop = heightPart * percent.min + 'px';
+      if (percent.max !== undefined) this.element.style.marginBottom = heightPart * (100 - percent.max) + 'px';
+    } else {
+      if (percent.min !== undefined) this.element.style.marginLeft = percent.min + '%';
+      if (percent.max !== undefined) this.element.style.marginRight = 100 - percent.max + '%'
     }
-
-    updatePosition(isVertical: boolean, percent: number, percentRanged: number, rootElement?: HTMLElement) {
-        if (isVertical) {
-            const heightPart = (rootElement ? rootElement.clientHeight - 60 : this.element.parentElement.clientHeight) / 100;
-            this.element.style.marginBottom = heightPart * percent + 'px';
-            this.element.style.marginTop = heightPart * (100 - percentRanged) + 'px';
-        } else {
-            this.element.style.marginLeft = percent + '%';
-            this.element.style.marginRight = 100 - percentRanged + '%'
-        }
-    }
-
+  }
 }
 
 export default Range;
--- src/slider/view/scale/Scale.ts
+++ src/slider/view/scale/Scale.ts
@@ -1,22 +1,46 @@
 import IViewElement from "../IViewElement";
 import CssClassUtil from "../CssClassUtil";
+import Observer from "../../observer/Observer";
+import SliderEvent from "../../observer/SliderEvent";
 
-class Scale implements IViewElement{
-    element: HTMLElement;
+class Scale extends Observer implements IViewElement {
+  element: HTMLElement;
 
-    buildHtml(isVertical: boolean, linesCount:number): HTMLElement {
-        console.log(linesCount)
-        this.element = document.createElement('div');
-        CssClassUtil.initClass(this, isVertical);
-        let line;
+  buildHtml(isVertical: boolean): HTMLElement {
+    this.element = document.createElement('div');
+    CssClassUtil.initClass(this, isVertical);
+    this.element.addEventListener('click', this.handleScaleClick);
+    return this.element;
+  }
 
-        for (let i = 0; i < linesCount - 2; i++){
-            line = document.createElement('div');
-            CssClassUtil.initHtmlClass(line, isVertical, 'scale-line');
-            this.element.append(line);
-        }
-        return this.element;
+  buildLineHtml(isVertical: boolean): HTMLDivElement {
+    const line = document.createElement('div');
+    CssClassUtil.initHtmlClass(line, isVertical, 'scale-line');
+    return line;
+  }
+
+  updateLines(count: number, isVertical: boolean) {
+    const diff = count - 2 - this.element.childElementCount;
+    if (diff > 0) {
+      const fragment = document.createDocumentFragment();
+      for (let i = 0; i < diff; i++) {
+        fragment.append(this.buildLineHtml(isVertical));
+      }
+      this.element.append(fragment);
+    } else if (diff < 0) {
+      for (let i = 0; i < -diff; i++) {
+        this.element.lastElementChild.remove();
+      }
     }
+  }
+
+  toggle() {
+    CssClassUtil.toggleHidden(this);
+  }
+
+  private handleScaleClick = (event: MouseEvent) => {
+    this.notify(SliderEvent.sliderClick, {x: event.offsetX, y: event.offsetY});
+  }
 }
 
 export default Scale;
